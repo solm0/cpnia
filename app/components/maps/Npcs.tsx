16bda3448c1532253092cbfa018bf7704b33aeb3
정솔미 <solmi-@kookmin.ac.kr>
2025-10-05 21:23:30 +0900

    npc, 게임포탈 상태 라벨

--- app/components/maps/Npcs.tsx
+++ app/components/maps/Npcs.tsx
@@ -4,6 +4,7 @@ import ChatNpc from "./ChatNpc";
 import { RigidBody } from "@react-three/rapier";
 import { useState } from "react";
 import { chatNpcProp } from "@/app/lib/data/positions/chatNpcs";
+import { Billboard, Image } from "@react-three/drei";
 
 export default function Npcs({
   worldKey, setActiveNpc, setIsChatOpen, chatNpc
@@ -14,46 +15,76 @@ export default function Npcs({
   chatNpc: chatNpcProp;
 }) {
   const [hoveredNpc, setHoveredNpc] = useState<string | null>(null);
-  
+
   return (
     <>
       {/* 맵 npc */}
       {mapNpcs[worldKey].map(npc => 
-        <RigidBody
+        <group
           key={npc.name}
-          position={npc.position}
-          rotation={npc.rotation}
-          scale={npc.scale ?? 8}
-          colliders={'cuboid'}
-          type="fixed"
         >
-          <MapNpc
-            key={npc.name}
-            name={npc.name}
-            hoveredNpc={hoveredNpc}
-            setHoveredNpc={setHoveredNpc}
-            setActiveNpc={setActiveNpc}
-            model={npc.model}
-            closeIsChatOpen={setIsChatOpen}
-          />
-        </RigidBody>
+          <Billboard position={
+            npc.name != '피자커팅기'
+              ? [npc.position[0],npc.position[1]+7,npc.position[2]]
+              : [npc.position[0]-4,npc.position[1]+25,npc.position[2]+1]
+          }>
+            <Image
+              url={npc.name != '피자커팅기'
+                ? "/images/threedots.png"
+                : "/images/exclaim.png"
+              }
+              scale={
+                npc.name != '피자커팅기'
+                  ? [2,2]
+                  : [4,4]
+              }
+              transparent
+            />
+          </Billboard>
+          <RigidBody
+            position={npc.position}
+            rotation={npc.rotation}
+            scale={npc.scale ?? 8}
+            colliders={'cuboid'}
+            type="fixed"
+          >
+            <MapNpc
+              key={npc.name}
+              name={npc.name}
+              hoveredNpc={hoveredNpc}
+              setHoveredNpc={setHoveredNpc}
+              setActiveNpc={setActiveNpc}
+              model={npc.model}
+              closeIsChatOpen={setIsChatOpen}
+            />
+          </RigidBody>
+        </group>
       )}
 
       {/* 챗 npc */}
-      <RigidBody
-        position={chatNpc.position}
-        rotation={chatNpc.rotation}
-        colliders={'cuboid'}
-        type="fixed"
-      >
-        <ChatNpc
-          name={chatNpc.name}
-          hoveredNpc={hoveredNpc}
-          setHoveredNpc={setHoveredNpc}
-          setIsChatOpen={setIsChatOpen}
-          closeActiveNpc={setActiveNpc}
-        />
-      </RigidBody>
+      <group>
+        <Billboard position={[chatNpc.position[0],chatNpc.position[1]+7,chatNpc.position[2]]}>
+          <Image
+            url="/images/exclaim.png"
+            scale={[2,2]}
+            transparent
+          />
+        </Billboard>
+        <RigidBody
+          position={chatNpc.position}
+          rotation={chatNpc.rotation}
+          colliders={'cuboid'}
+          type="fixed"
+        >
+          <ChatNpc
+            name={chatNpc.name}
+            hoveredNpc={hoveredNpc}
+            setHoveredNpc={setHoveredNpc}
+            setIsChatOpen={setIsChatOpen}
+            closeActiveNpc={setActiveNpc}
+          />
+        </RigidBody>
+      </group>
     </>
   )
 }
--- app/components/maps/Portals.tsx
+++ app/components/maps/Portals.tsx
@@ -2,6 +2,7 @@ import { RigidBody } from "@react-three/rapier";
 import { gamePortals } from "@/app/lib/data/positions/gamePortals";
 import GamePortalLayout from "./interfaces/GamePortalLayout";
 import PlaceHolder from "../util/PlaceHolder";
+import { useGameStore } from "@/app/lib/state/gameState";
 
 export default function Portals({
   worldKey,
@@ -9,11 +10,24 @@ export default function Portals({
   worldKey: string;
 }) {
   const visibleGamePortals = gamePortals[worldKey].filter(portal => portal.position != null && portal.rotation != null);
+
+  const gameState = useGameStore(state => state.worlds[worldKey].games)
+
+  let stage;
+  if (gameState['game1'] === false) {
+    stage = 'game1';
+  } else if (gameState['game2'] === false) {
+    stage = 'game2'
+  } else if (gameState['game3'] === false) {
+    stage = 'game3'
+  } else {
+    stage = 'unknown';
+  }
   
   return (
     <>
       {/* 게임 포탈 */}
-      {visibleGamePortals.map(game => 
+      {visibleGamePortals.map((game, idx) => 
         <RigidBody
           key={game.gameKey}
           position={game.position}
@@ -24,6 +38,7 @@ export default function Portals({
             label={game.label}
             worldKey={worldKey}
             gameKey={game.gameKey}
+            locked={Number(game.gameKey.slice(-1)) > Number(stage.slice(-1))}
           >
             <PlaceHolder />
           </GamePortalLayout>
