87095bae5be049db245ac9c77f90785d784f55a2
정솔미 <solmi-@kookmin.ac.kr>
2025-10-19 11:22:15 +0900

    하......cylinder 돌아가는거오류 고침 index 문제였음...

--- app/components/games/games/W1G2.tsx
+++ app/components/games/games/W1G2.tsx
@@ -1,7 +1,41 @@
 import Scene from "../../util/Scene";
 import GameMenu from "../interfaces/GameMenu";
-import { useEffect, useState } from "react";
+import { RefObject, useRef, useState } from "react";
 import SlotGame from "./W1G2/SlotGame";
+import Button from "../../util/Button";
+
+function Ui({
+  success, motionPhase, onRoundEnd,
+}: {
+  success: boolean | null;
+  motionPhase: RefObject<'idle' | 'toSide' | 'handle' | 'toFront' | 'cylinder' | 'done'>;
+  onRoundEnd: (success: boolean) => void;
+}) {
+  if (motionPhase.current === 'idle') {
+    return (
+      <div className="w-screen h-screen pt-60 pointer-events-none absolute">
+        <Button
+          worldKey="time"
+          label="시작"
+          autoFocus={true}
+          onClick={() => {
+            motionPhase.current = 'toSide'
+            console.log('ff')
+          }}
+        />
+      </div>
+    )
+  } else if (motionPhase.current === 'done') {
+    console.log(success);
+    if (success === true) {
+      console.log('성공')
+      return <div>성공</div>
+    } else {
+      console.log('실패')
+      return <div>실패</div>
+    }
+  }
+}
 
 export default function W1G2({
   worldKey, gameKey, onGameEnd
@@ -12,6 +46,9 @@ export default function W1G2({
 }) {
   const [trial, setTrial] = useState(0);
   const [successCount, setSuccessCount] = useState(0);
+
+  const motionPhase = useRef<'idle' | 'toSide' | 'handle' | 'toFront' | 'cylinder' | 'done'>('idle');
+  const [success, setSuccess] = useState<boolean | null>(null);
 
   function onRoundEnd(success: boolean) {
     // 3개의 각도 조합이 맞음 => 성공횟수 + 1
@@ -33,22 +70,30 @@ export default function W1G2({
   
   return (
     <main className="w-full h-full">
-      {/* 게임 */}
-      <Scene>
-        <SlotGame
-          onRoundEnd={onRoundEnd}
-        />
-        
-        {/* 빛 */}
-        <directionalLight intensity={3} position={[0,30,40]} />
-      </Scene>
-
+      
       {/* 게임 인터페이스 */}
       <GameMenu
         worldKey={worldKey}
         gameKey={gameKey}
         score={successCount * 10}
       />
+      <Ui
+        success={success}
+        motionPhase={motionPhase}
+        onRoundEnd={onRoundEnd}
+      />
+
+      {/* 게임 */}
+      <Scene>
+        <SlotGame
+          onRoundEnd={onRoundEnd}
+          motionPhase={motionPhase}
+          setSuccess={setSuccess}
+        />
+        
+        {/* 빛 */}
+        <directionalLight intensity={3} position={[0,30,40]} />
+      </Scene>
     </main>
   )
 }
--- app/components/games/games/W1G2/SlotGame.tsx
+++ app/components/games/games/W1G2/SlotGame.tsx
@@ -1,13 +1,16 @@
-import { useGLTF } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
+import Button from "@/app/components/util/Button";
+import { Html, useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { Object3D, Vector3 } from "three"
-import { degToRad } from "three/src/math/MathUtils.js";
+import { degToRad, MathUtils } from "three/src/math/MathUtils.js";
 
 export default function SlotGame({
-  onRoundEnd,
+  onRoundEnd, motionPhase, setSuccess,
 }: {
   onRoundEnd: (success: boolean) => void;
+  motionPhase: RefObject<'idle' | 'toSide' | 'handle' | 'toFront' | 'cylinder' | 'done'>;
+  setSuccess: (success: boolean) => void;
 }) {
   const main = useGLTF('/models/pachinko/main.glb').scene;
   const cylinders: Object3D[] = [
@@ -24,7 +27,7 @@ export default function SlotGame({
     camera.lookAt(0, 5, 0);
   }, [camera]);
 
-  const groupRotation = [0, Math.PI, 0] as [number, number, number];
+  const groupRotation = useRef<[number, number, number]>([0, Math.PI, 0]);
 
   const mainCenter = new Vector3(0,0,0);
   const handleCenter = new Vector3(
@@ -40,6 +43,7 @@ export default function SlotGame({
   const cylinderGap = 0.56;
   const correction = 40;
   const cylinderInitRotationX = useRef(degToRad(-correction));
+  const cylinderRefs = useRef<Object3D[]>([]);
 
   // 랜덤 3개의 각도 뽑기
   const angleObjMap: {angle:number, obj:string}[][] = [
@@ -69,64 +73,102 @@ export default function SlotGame({
     reel => reel[Math.floor(Math.random() * reel.length)]
   );
 
-  const rotCount = 5;
+  const spins = 5;
+  const finalRot = randomAngles.map(item => item.angle + 360 * spins);
   const success = randomAngles.every(a => a.obj === randomAngles[0].obj);
-  const finalRot = randomAngles.forEach(item => -(item.angle + correction + 360 * rotCount));
 
-  // motionPhase = useRef<'idle' | 'toSide' | 'handle' | 'toFront' | 'cylinder' | 'done'>('idle');
-  // useFrame
-  // 버튼 클릭하면 toSide
-  // function toSide { if(motionPhase === 'idle') ..., motionPhase.current = 'handle'}
-  // ...
-  // handle - finalRot[cylinder index] 만큼 돌아가고 페이드아웃
+  const rotationProgress = useRef(0);
+
+  useFrame((_, delta) => {
+    if (motionPhase.current === 'toSide') {
+      // groupRotation.y값이 Math.PI에서 Math.PI/1.5
+      groupRotation.current[1] = Math.max(groupRotation.current[1] - delta, Math.PI / 1.5);
+      if (groupRotation.current[1] <= Math.PI / 1.5) {
+        motionPhase.current = 'handle';
+      }
+    }
+    if (motionPhase.current === 'handle') {
+      // handleRotation.x 값이 degToRad(5)에서 degToRad(-85)로 갔다가 돌아옴.
+      motionPhase.current = 'toFront';
+    }
+    if (motionPhase.current === 'toFront') {
+      // groupRotation.y값이 Math.PI/1.5에서 // groupRotation.y값이 Math.PI에서 Math.PI
+      groupRotation.current[1] = Math.max(groupRotation.current[1] + delta, Math.PI);
+      if (groupRotation.current[1] >= Math.PI) {
+        motionPhase.current = 'cylinder';
+      }
+    }
+    if (motionPhase.current === 'cylinder') {
+      if (motionPhase.current === 'cylinder' && rotationProgress.current === 0) {
+        cylinderRefs.current.forEach(c => {
+          c.rotation.x = degToRad(-correction);
+        });
+      }
+
+      console.log('cylinder 0 initial rot.x', cylinderRefs.current[0]?.rotation.x);
+
+      // 각 cylinder들이 저마다의 finalRot[cylinder index]만큼 돌아가고 멈춘다.
+      rotationProgress.current = Math.min(rotationProgress.current + delta * 1, 1);
+
+      cylinderRefs.current.forEach((c, i) => {
+        const startRot = degToRad(-correction);
+        const endRot = degToRad(finalRot[i]-correction);
+        console.log(randomAngles[0].obj, randomAngles[1].obj, randomAngles[2].obj)
+        c.rotation.x = MathUtils.lerp(startRot, endRot, rotationProgress.current);
+      });
+
+      if (rotationProgress.current >= 1) {
+        motionPhase.current = 'done';
+      }
+    }
+  })
 
   return (
-    <group
-      rotation={groupRotation}
-    >
-      {/* 본체 */}
-      <primitive
-        object={main}
-        position={mainCenter}
-      />
-      
-      {/* 원통 1,2,3 */}
-      {cylinders.map((c, index) => {
-        const moveX = index === 0
-          ? -cylinderGap
-          : index === 1 
-            ? 0
-            : index === 2
-              ? cylinderGap
-              : 0
-        return (
-          <primitive
-            key={index}
-            object={c}
-            rotation={[
-              cylinderInitRotationX.current,
-              0,
-              0
-            ]}
-            position={[
-              cylinderCenter.x + moveX,
-              cylinderCenter.y,
-              cylinderCenter.z
-            ]}
-          />
-        )
-      })}
+    <>
+      <group rotation={groupRotation.current}>
+        {/* 본체 */}
+        <primitive
+          object={main}
+          position={mainCenter}
+        />
+        
+        {/* 원통 1,2,3 */}
+        {cylinders.map((c, index) => {
+          const moveX = index === 0
+            ? -cylinderGap
+            : index === 1 
+              ? 0
+              : index === 2
+                ? cylinderGap
+                : 0
+          return (
+            <primitive
+              key={index}
+              ref={(el: Object3D) => {
+                if (el) cylinderRefs.current[2-index] = el;
+              }}
+              object={c}
+              rotation={[cylinderInitRotationX.current, 0, 0]}
+              position={[
+                cylinderCenter.x + moveX,
+                cylinderCenter.y,
+                cylinderCenter.z
+              ]}
+            />
+          )
+        })}
 
-      {/* 손잡이 */}
-      <primitive
-        object={handle}
-        rotation={[degToRad(5), 0, 0]}
-        position={[
-          handleCenter.x,
-          handleCenter.y,
-          handleCenter.z
-        ]}
-      />
-    </group>
+        {/* 손잡이 */}
+        <primitive
+          object={handle}
+          rotation={[degToRad(5), 0, 0]}
+          position={[
+            handleCenter.x,
+            handleCenter.y,
+            handleCenter.z
+          ]}
+        />
+      </group>
+    </>
   )
 }
