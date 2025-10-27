e758ba3f269ffe721c274e6499c22f4c3a792f29
정솔미 <solmi-@kookmin.ac.kr>
2025-10-27 13:48:16 +0900

    세번째게임시작

--- app/components/games/games/W3G1.tsx
+++ app/components/games/games/W3G1.tsx
@@ -1,8 +1,116 @@
-import PlaceHolderGame from "./PlaceHolderGame";
 import Scene from "../../util/Scene";
-import GameMenu from "../interfaces/GameMenu";
-import { useState } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { RefObject, useEffect, useRef, useState } from "react";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useTimerRef } from "./W3G3/useTimerRef";
+import FullScreenModal from "../../util/FullScreenModal";
+import Button from "../../util/Button";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useGamepadControls } from "@/app/lib/hooks/useGamepadControls";
+import { useKeyboardControls } from "@/app/lib/hooks/useKeyboardControls";
+
+function Player({
+  timerRef, width,
+}: {
+  timerRef: RefObject<{
+    startTime: number;
+    elapsed: number;
+    running: boolean;
+  }>;
+  width: number
+}) {
+  const avatar = useGLTF('/models/avatars/default.gltf').scene;
+  const [pos, setPos] = useState(1);
+
+  // 키보드 컨트롤
+  const pressed = useKeyboardControls();
+  const gamepad = useGamepadControls();
+
+  useEffect(() => {
+    const deadzone = 0.5;
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (pressed.current.has("KeyD") || gamepad.current.axes[0] > deadzone) {
+        setPos(prev => {
+          if (prev <= 2) {
+            return prev + 1;
+          }
+          return prev;
+        });
+      } else if (pressed.current.has("KeyA") || gamepad.current.axes[0] < -deadzone) {
+        setPos(prev => {
+          if (prev >= 1) {
+            return prev - 1;
+          }
+          return prev;
+        });
+      }
+    };
+  
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  useFrame(() => {
+    // --- 타이머 ---
+    if (timerRef.current.running) {
+      timerRef.current.elapsed = (performance.now() - timerRef.current.startTime) / length;
+    }
+  })
+
+  return (
+    <primitive
+      object={avatar}
+      scale={width * 0.1}
+      position={[pos * 3, 0, 0]}
+    />
+  )
+}
+
+function GameScene({
+  width, timerRef
+}: {
+  width: number;
+  timerRef: RefObject<{
+    startTime: number;
+    elapsed: number;
+    running: boolean;
+  }>;
+}) {
+  const { camera } = useThree();
+
+  // 초기 카메라
+  useEffect(() => {
+    camera.position.set(0,5,10);
+    camera.rotation.set(0,0,0);
+    camera.lookAt(0, 5, 0);
+  }, [camera]);
+
+  useFrame(() => {
+    
+
+    // groupRef의 z축 이동 속도에 width 곱하기
+  })
+
+  return (
+    <>
+      {/* 큐브들 */}
+
+      {/* 플레이어 */}
+      <Player timerRef={timerRef} width={width} />
+
+      <OrbitControls minDistance={30} maxDistance={100} />
+    </>
+  )
+}
+
+function Ui({
+  life
+}: {
+  life: number;
+}) {
+  return (
+    <div className="absolute top-0 left-0 w-auto h-10 bg-amber-600">{life}</div>
+  )
+}
 
 export default function W3G1({
   worldKey, gameKey, onGameEnd
@@ -11,27 +119,45 @@ export default function W3G1({
   gameKey: string;
   onGameEnd: (success: boolean) => void;
 }) {
-  // 게임마다 다른 게임 상태 저장. 점수만 Game으로 올려줌.
-  const [click, setClick] = useState(0);
+  const [life, setLife] = useState(10);
+  const width = 50;
+  const { timerRef, start } = useTimerRef();
+  const [hasStarted, setHasStarted] = useState(false);
+
+  useEffect(() => {
+    if (life <= 0) onGameEnd(false);
+  }, [life])
   
   return (
     <main className="w-full h-full">
       {/* 게임 */}
       <Scene>
-        <PlaceHolderGame
-          click={click}
-          setClick={setClick}
-          onGameEnd={onGameEnd}
+        <GameScene
+          width={width}
+          timerRef={timerRef}
         />
-        <OrbitControls minDistance={30} maxDistance={100} />
+        
       </Scene>
 
       {/* 게임 인터페이스 */}
-      <GameMenu
-        worldKey={worldKey}
-        gameKey={gameKey}
-        score={click}
+      <Ui
+        life={life}
       />
+
+      {!hasStarted &&
+        <FullScreenModal>
+          <p>핑크 타일만 터치하세요</p>
+          <Button
+            onClick={() => {
+              setHasStarted(true);
+              start();
+            }}
+            label="시작하기"
+            worldKey={worldKey}
+            autoFocus={true}
+          />
+        </FullScreenModal>
+      }
     </main>
   )
 }
