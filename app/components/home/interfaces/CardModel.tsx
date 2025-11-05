2d6465befa8abad1b03d84e333918f3c0f68d080
정솔미 <solmi-@kookmin.ac.kr>
2025-11-05 23:18:09 +0900

    시민권교체

--- app/components/home/interfaces/CardModel.tsx
+++ app/components/home/interfaces/CardModel.tsx
@@ -9,7 +9,7 @@ export default function CardModel({
   worldKey: string | null;
   isCompleted?: boolean;
 }) {
-  const card = useGLTF(`/models/citizenship/${worldKey}.glb`).scene;
+  const card = useGLTF(`/models/citizenship/${worldKey}.gltf`).scene;
 
   return (
     <Suspense fallback={<Loader />}>
@@ -17,7 +17,8 @@ export default function CardModel({
         <primitive
           object={card}
           position={[0,0,0]}
-          rotation={[degToRad(90), degToRad(30), degToRad(60)]}
+          rotation={[degToRad(-10), degToRad(-30), degToRad(70)]}
+          scale={20}
         />
         <OrbitControls
           enableZoom={false}
@@ -25,8 +26,8 @@ export default function CardModel({
           maxPolarAngle={Math.PI / 2}
         />
         {isCompleted && <>
-          <directionalLight intensity={3} position={[0, 50, 0]} />
-          <directionalLight intensity={3} position={[0, 10, -10]} />
+          <directionalLight intensity={3} position={[-10, 30, 10]} />
+          <directionalLight intensity={3} position={[-10, 30, -30]} />
         </>}
       </SmallScene>
     </Suspense>
