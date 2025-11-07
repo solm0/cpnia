ffea3da8d4a612930e6df37be0290c61b751b46f
정솔미 <solmi-@kookmin.ac.kr>
2025-11-07 13:11:37 +0900

    엔트로피3

--- app/components/maps/Lights.tsx
+++ app/components/maps/Lights.tsx
@@ -117,8 +117,31 @@ export function EntropyLights() {
     <>
       <directionalLight
         intensity={10}
-        position={[10,80,10]} castShadow receiveShadow  />
-      <directionalLight intensity={10} color={'blue'} position={[10,80,50]} castShadow receiveShadow />
+        position={[10,80,10]}
+        castShadow
+        shadow-mapSize-width={2048}   // higher = sharper shadow
+        shadow-mapSize-height={2048}
+        shadow-camera-left={-500}      // expand shadow frustum
+        shadow-camera-right={500}
+        shadow-camera-top={500}
+        shadow-camera-bottom={-500}
+        shadow-camera-near={0.1}      // near/far clipping
+        shadow-camera-far={1000}
+      />
+      <directionalLight
+        intensity={10}
+        color={'blue'}
+        position={[10,80,50]}
+        castShadow
+        shadow-mapSize-width={2048}   // higher = sharper shadow
+        shadow-mapSize-height={2048}
+        shadow-camera-left={-500}      // expand shadow frustum
+        shadow-camera-right={500}
+        shadow-camera-top={500}
+        shadow-camera-bottom={-500}
+        shadow-camera-near={0.1}      // near/far clipping
+        shadow-camera-far={1000}
+      />
     </>
   )
 }
